d93806c45a6d2ef96c5500e46dda13dcc5f9cf0a
Catherine Smith <catsmith24601@gmail.com>
2021-03-22 12:32:10 -0400

    created index.html, script.js, and style.css; add Moment function to script.js, updated README.md to include user story and acceptance criteria

---
+++ script.js
@@ -0,0 +1,13 @@
+// Globally-scoped variables
+
+
+
+// Moment to display current day and date
+var currentDay = moment().format("dddd, MMMM Do, YYYY");
+$("#currentDay").text(currentDay);
+
+// jQuery UIs
+
+
+
+// Other functions
