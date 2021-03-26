bae0da315f6d3b05fc5f2040769165b13554cee6
Catherine Smith <catsmith24601@gmail.com>
2021-03-26 14:26:02 -0400

    added index2.html and script2.js to play with table being created and appended via script instead of html, modified original html, css, and js to add event listener for save button and to work with local storage

--- script.js
+++ script.js
@@ -13,49 +13,119 @@ var updateDateAndTime = function() {
 updateDateAndTime();
 setInterval(updateDateAndTime, 1000);
 
-// jQuery UIs
+
+function setLocalStorage(objKey, objValue) {
+    var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
+    dailySchedule[objKey] = objValue;
+    localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
+}
+
+
+$(document).ready(function() {
+    var scheduledItems = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
+    var taskItem = $(".userTask")
+    $.each(taskItem, function(index, object) {
+        var object = $(object);
+        var dataValue = object.attr("value");
+
+        $.each(scheduledItems, function (key, value) {
+            if (key == dataValue) {
+                object.text(value);
+            }
+        });
+    });
+});
 
 
 
+init();
+function init() {
+    for (var i = 9; i < 18; i++) {
+        $("#" + i + "text").val(localStorage.getItem(i));
+    }
+};
 
 
 // // Event listener for save button
-// $()
+// $('.saveBtn').click(function (e) {
+//     e.preventDefault();
+//     // Identifies what row to save to local storage
+//     var id = $(this).data('timeslot');
+//     // Saves the row and task item to local storage
+//     let task = {
+//         timeslot: $(this).data('timeslot'),
+//         message: $('#' + id + 'text').val(),
+//     };
+//     localStorage.setItem(task.timeslot, task.message);
+//     console.log("saved") 
+//     // so saved button is working but local storage is not
+// });
+
+
+$('.saveBtn').on('click', function(){
+
+    $('input[type="text"]').each(function(){    
+        var id = $(this).attr('id');
+        var value = $(this).val();
+       localStorage.setItem(id, value);
+
+    });   
+// });
+
+// $('#load').on('click', function(){
+//     $('input[type="text"]').each(function(){    
+//         var id = $(this).attr('id');
+//         var value = localStorage.getItem(id);
+
+//         $(this).val(value);
+
+//     }); 
+// });
+
+
+// //takes 'event description' in modal and passes to user interface
+// $('#saveBtn').click(function() {
+//     // gets value of user input
+//     var eventInformation = $('#eventDescription').val(); 
+    
+//     // to get HTML of <span> #chosenTime' field of modal. This returns a few HTML elements but I want index[1]
+//     var chosenTimeHTML = $(this).parent().siblings().prev()[1].querySelector('span');
+
+//     //cast chosenTimeHTML to jQuery object
+//     var chosenTimeHTML = $(chosenTimeHTML);
+
+//     // to get data-hourValue data attribute 
+//     var dataAttribute = chosenTimeHTML.attr('data-hourValue');
+
+//     //loops through each row to see if data-value == data-hourValue of current chosen row
+//     $(".eventInfo").each(function(i, object) {
+//         var object = $(object);   //converts to jQuery object
+
+//         // if data-value of .eventInfo == dataAttribute, set text of input box to that cell in scheduler
+//         if (object.attr('value') == dataAttribute) {
+//             object.html(eventInformation);
+            
+//             //set values to localStorage
+//             setLocalStorage(dataAttribute, eventInformation);
+//         }
+//     });
+// });
 
 
 
-// // Other functions
-// var currentHour = ;
-
-// // Array with business hours
-// var hours = [
-//     "9:00 AM",
-//     "10:00 AM",
-//     "11:00 AM",
-//     "12:00 PM",
-//     "1:00 PM",
-//     "2:00 PM",
-//     "3:00 PM",
-//     "4:00 PM",
-//     "5:00 PM",
-// ];
 
 
+// Color coding table rows based on past, present, and future hours.
+// 
+$(".timeslot").each(function() {
+    var time = $(this).data("time");                   
+    var now = moment().hour();
 
-
-// // Color coding table rows based on past, present, and future hours (9 hours total, starting at 9 o'clock so up to 18)- for loop????
-// for (var i = 9, i < 18, i++) {
-//     if (currentHour > j) {
-
-//     }
-
-//     if (currentHour === j) {
-
-//     }
-
-//     if (currentHour < j) {
-
-//     }
-// };
-
-
+    if (time < now) {
+        $(this).parent().setAtrribute("style", "background-color: #d3d3d3");
+    } else if (time = now) {
+        $(this).parent().setAtrribute("style", "background-color: #ff6961");
+    } else if (time > now) {
+        $(this).parent().setAtrribute("style", "background-color: #77dd77");
+    }
+});
---
+++ script2.js
@@ -0,0 +1,102 @@
+
+  $(document).ready(function () {
+    var currentHour = moment().hour();
+
+    // Displays the current date and time in the Jumbotron
+    var update = function () {
+        $('#currentDay').text(moment().format('dddd, MMMM Do'));
+        $('#currentTime').text(moment().format('h:mm:ss a'));
+    };
+    update();
+    setInterval(update, 1000);
+
+    // Array with hours for scheduler
+    const hours = [
+        '9:00 AM - 10:00 AM',
+        '10:00 AM - 11:00 AM',
+        '11:00 AM - 12:00 PM',
+        '12:00 PM - 1:00 PM',
+        '1:00 PM - 2:00 PM',
+        '2:00 PM - 3:00 PM',
+        '3:00 PM - 4:00 PM',
+        '4:00 PM - 5:00 PM',
+        '5:00 PM - 6:00 PM',
+    ];
+
+    //---------------
+    // Loop to make table based on length of hours array
+    //---------------
+    for (var i = 0; i < hours.length; i++) {
+        //Assign current hour in 24-hr notation to a variable
+        var dataHour = [i + 9];
+        // Create a row
+        var tRow = $('<tr class="hourColor">');
+        // Populate first column with hours array along with dataHour
+        var hourRow = $(
+            `<td class="align-middle"><h3 class="time" id="${hours[i]}" data-hour="${dataHour}">${hours[i]}</h3></td>`
+        );
+        // Create a column with a textbox along with dataHour
+        var task = $(
+            `<td class="align-middle"><textarea class="form-control taskText" id="${dataHour}text" rows="3"></textarea></td>`
+        );
+        // Create a column with a save button along with dataHour
+        var save = $(
+            `<td class="align-middle"><i class="far fa-save fa-3x saveBtn" data-hour="${dataHour}"></i></td>`
+        );
+        // Append the newly created table data to the table row
+        tRow.append(hourRow, task, save);
+        // Append the table row to the table body
+        $('tbody').append(tRow);
+    }
+
+    //---------------
+    // Add content of text area to local storage on save, and display
+    //---------------
+    // Index the textareas in the html, and assigns local storage values
+    init();
+    function init() {
+        for (var k = 9; k < 18; k++) {
+            // Populates textareas with existing local storage
+            $('#' + k + 'text').val(localStorage.getItem(k));
+        }
+    }
+
+    // Event listener on save button
+    $('.saveBtn').click(function (e) {
+        e.preventDefault();
+        // Identifies what row is being saved to local storage
+        var id = $(this).data('hour');
+        // Saves the row and todo item into local storage
+        let task = {
+            hour: $(this).data('hour'),
+            message: $('#' + id + 'text').val(),
+        };
+        localStorage.setItem(task.hour, task.message);
+    });
+
+    //---------------
+    // Color code the hour blocks based on time of day and whether is in past, present, or future
+    //---------------
+    // Loop across each row of the table, starting at 9 (for 9am)
+    for (var j = 9; j < 18; j++) {
+        // If the current hour is less than the loop iteration value (starting at 9), make the background one color
+        if (currentHour > j) {
+            $('.hourColor')
+                .eq(j - 9)
+                .css('background-color', '#d3d3d3');
+        }
+        // If the current hour is equal to the loop iteration value (starting at 9), make the background another color
+        if (currentHour === j) {
+            $('.hourColor')
+                .eq(j - 9)
+                .css('background-color', '#ff6961');
+        }
+        // If the current hour is more than the loop iteration value (starting at 9), make the background a third color
+        if (currentHour < j) {
+            $('.hourColor')
+                .eq(j - 9)
+                .css('background-color', '#77dd77');
+        }
+    }
+});
+
